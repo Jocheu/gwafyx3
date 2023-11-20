96424634f6f6b65e8162ac5dee80661d6c9bbe15
Jocheu <joachim.psuty.lipski@gmail.com>
2023-11-20 12:11:53 +0100

    blokowanie podwójnego zaznaczenia tego samego buttona

--- app/src/main/java/com/example/gwafyx3/MainActivity.kt
+++ app/src/main/java/com/example/gwafyx3/MainActivity.kt
@@ -3,30 +3,46 @@ package com.example.gwafyx3
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Range
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var a = arrayOf(findViewById<ImageButton>(R.id.img1),findViewById<ImageButton>(R.id.img2),findViewById<ImageButton>(R.id.img3), findViewById<ImageButton>(R.id.img4),findViewById<ImageButton>(R.id.img5))
+        var INF = 9999999
+        var a = arrayOf(findViewById<ImageButton>(R.id.img0),findViewById<ImageButton>(R.id.img1),findViewById<ImageButton>(R.id.img2), findViewById<ImageButton>(R.id.img3),findViewById<ImageButton>(R.id.img4))
         var b = arrayListOf<ImageButton>()
+        var txt = findViewById<TextView>(R.id.textView)
+        var tablica = Array(5) {Array (5){INF} }
+        for(i in 0..tablica.size -1){
+            tablica[i][i] = 0
+        }
         for (elements in a){
             elements.setOnClickListener(){
+
                 if (b.size<=1){
-                    b.add(elements)
+                    if(b.size==0) {
+                        b.add(elements)
+                    }else if(b[0]!=elements){
+                        b.add(elements)
+                    }
+                    else{
+
+                    }
+
+
                 }
                 else{
                     b = arrayListOf()
                     b.add(elements)
                 }
-
+                txt.text = Arrays.deepToString(arrayOf(b))
+                /*dodaj system blokowania dwa razy tego samego przycisku */
                 if(b.size<=1){
                     findViewById<TextView>(R.id.txtBtn1).text= resources.getResourceEntryName(elements.id).removePrefix("img")
                     findViewById<TextView>(R.id.txtBtn2).text = ""
@@ -41,12 +57,15 @@ class MainActivity : AppCompatActivity() {
                     alert.setView(inputText)
                     alert.setPositiveButton("Confirm", DialogInterface.OnClickListener { dialogInterface, i ->
                         findViewById<TextView>(R.id.textView).text = inputText.text
+                        tablica[resources.getResourceEntryName(b[0].id).removePrefix("img").toInt()][resources.getResourceEntryName(b[1].id).removePrefix("img").toInt()] = inputText.text.toString().toInt()
+                        txt.text = Arrays.deepToString(tablica)
                         dialogInterface.cancel()
                     })
                     alert.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialogInterface, i ->
                         dialogInterface.cancel()
                     })
                     alert.show()
+
 
                 }
             }
